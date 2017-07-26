349e416da5195a3f99070e85bfd3521b23c0e33d
Mohit Savaliya <mohitsavaliya111@gmail.com>
2017-07-27 02:13:24 +0530

    Create trie.java

---
+++ trie.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Trie {
+
+    private class TrieNode {
+        Map<Character, TrieNode> children;
+        boolean endOfWord;
+        public TrieNode() {
+            children = new HashMap<>();
+            endOfWord = false;
+        }
+    }
+
+    private final TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+
+ 
+    public void insert(String word) {
+        TrieNode current = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            TrieNode node = current.children.get(ch);
+            if (node == null) {
+                node = new TrieNode();
+                current.children.put(ch, node);
+            }
+            current = node;
+        }
+     
+        current.endOfWord = true;
+    }
+
+
+ 
+    public boolean searchRecursive(String word) {
+        return searchRecursive(root, word, 0);
+    }
+    private boolean searchRecursive(TrieNode current, String word, int index) {
+        if (index == word.length()) {
+            //return true of current's endOfWord is true else return false.
+            return current.endOfWord;
+        }
+        char ch = word.charAt(index);
+        TrieNode node = current.children.get(ch);
+        //if node does not exist for given char then return false
+        if (node == null) {
+            return false;
+        }
+        return searchRecursive(node, word, index + 1);
+    }
+
+
+}
